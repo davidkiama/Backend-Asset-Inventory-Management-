1c49a7c10c7754c59e751f38b2a03322d7ccab9d
davidkiama <davidkiama98@gmail.com>
2022-04-26 12:21:23 +0300

    Required models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Asset(models.Model):
+    creator = models.ForeignKey('manager', on_delete=models.CASCADE)
+    asset_name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.IntegerField()
+
+
+class Request (models.Model):
+    sender = models.ForeignKey('employee', on_delete=models.CASCADE)
+    status = models.CharField(max_length=50)
+    urgenty = models.CharField(max_length=50)
+    quantity = models.IntegerField()
