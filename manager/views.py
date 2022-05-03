cb6959a576df15116014ad8aa180cd8d071aa95c
davidkiama <davidkiama98@gmail.com>
2022-05-03 11:25:58 +0300

    Added a guard clause to restrict employees

--- manager/views.py
+++ manager/views.py
@@ -9,20 +9,24 @@ from rest_framework.authtoken.models import Token
 from django.http.response import JsonResponse
 
 # Create your views here.
+
+
 def manager_dashboard(request):
     '''
     Manager's dashboard view function
     '''
     company_assets = CompanyAsset.objects.all()
     context = {
-        'companyAssets':company_assets
+        'companyAssets': company_assets
     }
     return render(request, 'manager/dashboard_manager.html', context)
+
 
 class CompanyAssetsData(APIView):
     '''
     Class that handles company assets apis
     '''
+
     def get(self, request, format=None):
         '''
         Get function that display company assets data in JSON
@@ -32,13 +36,14 @@ class CompanyAssetsData(APIView):
             user_obj = Token.objects.get(key=token).user
 
         except:
-            return JsonResponse({'message': 'You must be logged in to view company assets'}, status=status.HTTP_401_UNAUTHORIZED) 
-        
+            return JsonResponse({'message': 'You must be logged in to view company assets'}, status=status.HTTP_401_UNAUTHORIZED)
+
         if user_obj.manager:
             company_assets = CompanyAsset.objects.all()
-            assets_serializer = CompanyAssetSerializer(company_assets, many=True)
+            assets_serializer = CompanyAssetSerializer(
+                company_assets, many=True)
         else:
-            return JsonResponse({'message': 'You must be logged in to view company assets'}, status=status.HTTP_401_UNAUTHORIZED)             
+            return JsonResponse({'message': 'You must be logged in to view company assets'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(assets_serializer.data)
 
     def post(self, request, format=None):
@@ -48,14 +53,17 @@ class CompanyAssetsData(APIView):
         try:
             token, created = Token.objects.get_or_create(user=request.user)
             user_obj = Token.objects.get(key=token).user
-
         except:
-            return JsonResponse({'message': 'You must be logged in to create company assets'}, status=status.HTTP_401_UNAUTHORIZED) 
+            return JsonResponse({'message': 'You must be logged in to create company assets'}, status=status.HTTP_401_UNAUTHORIZED)
 
-        assets_serializer =CompanyAssetSerializer(data=request.data)
-        if assets_serializer.is_valid():
-            assets_serializer.save()
-            return Response(assets_serializer.data, status=status.HTTP_201_CREATED)
+        if user_obj.manager:
+            assets_serializer = CompanyAssetSerializer(data=request.data)
+            if assets_serializer.is_valid():
+                assets_serializer.save()
+                return Response(assets_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse({'message': 'You must be a manager to create company assets'}, status=status.HTTP_401_UNAUTHORIZED)
+
         return Response(assets_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, format=None):
@@ -67,7 +75,5 @@ class CompanyAssetsData(APIView):
             user_obj = Token.objects.get(key=token).user
 
         except:
-            return JsonResponse({'message': 'You must be logged in to approve/reject company assets'}, status=status.HTTP_401_UNAUTHORIZED) 
+            return JsonResponse({'message': 'You must be logged in to approve/reject company assets'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response()
-
-
